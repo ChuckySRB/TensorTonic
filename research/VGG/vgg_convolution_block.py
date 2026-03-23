976930b9e34c8846ad2147decbc3544ec65a7d8f
ChuckySRB <c.petronic@a1.rs>
2026-03-23 17:50:04 +0100

    Started VGG

---
+++ research/VGG/vgg_convolution_block.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def vgg_conv_block(x: np.ndarray, num_convs: int, out_channels: int) -> np.ndarray:
+    """
+    Implement a VGG-style convolutional block.
+    """
+    # Your implementation here
+    input_dim = x.shape[-1]
+    
+    
+    pass
